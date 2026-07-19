dbaf7bfe3d76234fd7d78f8f788e7c01c11e62ca
mauvesea <79065840+mauvesea@users.noreply.github.com>
2026-07-19 00:47:50 -0300

    First batch of wild changes

--- data/wild/super_rod.asm
+++ data/wild/super_rod.asm
@@ -41,61 +41,61 @@ SuperRodData:
 
 .Group1:
 	db 2
-	db 15, TENTACOOL
-	db 15, POLIWAG
+	db 5, TENTACOOL
+	db 5, POLIWAG
 
 .Group2:
 	db 2
-	db 15, GOLDEEN
-	db 15, POLIWAG
+	db 5, GOLDEEN
+	db 5, POLIWAG
 
 .Group3:
 	db 3
-	db 15, PSYDUCK
-	db 15, GOLDEEN
-	db 15, KRABBY
+	db 5, PSYDUCK
+	db 5, GOLDEEN
+	db 5, KRABBY
 
 .Group4:
 	db 2
-	db 15, KRABBY
-	db 15, SHELLDER
+	db 5, KRABBY
+	db 5, SHELLDER
 
 .Group5:
 	db 2
-	db 23, POLIWHIRL
-	db 15, SLOWPOKE
+	db 5, POLIWHIRL
+	db 5, SLOWPOKE
 
 .Group6:
 	db 4
-	db 15, DRATINI
-	db 15, KRABBY
-	db 15, PSYDUCK
-	db 15, SLOWPOKE
+	db 5, DRATINI
+	db 5, KRABBY
+	db 5, PSYDUCK
+	db 5, SLOWPOKE
 
 .Group7:
 	db 4
 	db 5, TENTACOOL
-	db 15, KRABBY
-	db 15, GOLDEEN
-	db 15, MAGIKARP
+	db 5, KRABBY
+	db 5, GOLDEEN
+	db 5, MAGIKARP
 
 .Group8:
 	db 4
-	db 15, STARYU
-	db 15, HORSEA
-	db 15, SHELLDER
-	db 15, GOLDEEN
+	db 5, STARYU
+	db 5, HORSEA
+	db 5, SHELLDER
+	db 5, GOLDEEN
 
 .Group9:
 	db 4
-	db 23, SLOWBRO
-	db 23, SEAKING
-	db 23, KINGLER
-	db 23, SEADRA
+	db 5, SLOWBRO
+	db 5, SEAKING
+	db 5, KINGLER
+	db 5, SEADRA
 
 .Group10:
 	db 4
-	db 23, SEAKING
-	db 15, KRABBY
-	db 15, GOLDEEN
-	db 15, MAGIKARP
+	db 5, SEAKING
+	db 5, KRABBY
+	db 5, GOLDEEN
+	db 5, MAGIKARP
